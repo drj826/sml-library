df7084f788453effec4681a73685c1acf9d93eab
Don Johnson <drj826@acm.org>
2016-01-07 14:40:27 -0700

    Updated application scripts to fix a logging problem (resolves #9)

--- lib/update_sha_digests.pl
+++ lib/update_sha_digests.pl
@@ -1,12 +1,23 @@
 use Log::Log4perl;
 Log::Log4perl->init("../conf/log.conf");
+my $logger = Log::Log4perl::get_logger('sml.application');
 
 use lib "../../SML/lib";
 
 use SML::Library;
+use Time::Duration;
+
+my $begin = time();
 
 my $library = SML::Library->new(config_filename=>'library.conf');
 
+$logger->info("store SHA digest file");
+
 $library->store_sha_digest_file;
+
+my $end = time();
+my $duration = duration($end - $begin);
+
+$logger->info("store SHA digest file $duration");
 
 1;
